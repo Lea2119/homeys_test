0066aabc1158e768b37194fd66e1958778d24226
Léa Cassegrain <lea.csgn@gmail.com>
2024-11-04 22:47:18 +0100

    feat: 2 - display four notifications with different statuses and colors

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,48 @@
 <template>
   <main>
-    <BaseNotification>
-      <p>Operation completed successfully! 🌟</p>
-    </BaseNotification>
+    <div class="notifications-container">
+      <BaseNotification
+        v-for="notification in notifications"
+        :key="notification.id"
+        :status="notification.status"
+      >
+        <p>{{ notification.message }}</p>
+      </BaseNotification>
+    </div>
   </main>
 </template>
 
 <script setup>
 import BaseNotification from "@/components/BaseNotification.vue";
+
+const notifications = [
+  {
+    id: 1,
+    status: "error",
+    message: "An error occurred. Please try again. 🙀",
+  },
+  {
+    id: 2,
+    status: "success",
+    message: "Operation completed successfully! 🌟",
+  },
+  {
+    id: 3,
+    status: "warning",
+    message: "This is a warning. Please be careful! 🚨",
+  },
+  {
+    id: 4,
+    status: "info",
+    message: "Here is some information for you. 👩‍💻",
+  },
+];
 </script>
 
-<style scoped></style>
+<style scoped>
+.notifications-container {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+}
+</style>
--- src/components/BaseNotification.vue
+++ src/components/BaseNotification.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="notification">
+  <div :class="['notification', `notification--${props.status}`]">
     <div class="notification-header">
-      <img :src="successIcon" alt="icon" />
-      <p class="notification-title">Success</p>
+      <img :src="icon" alt="icon" />
+      <p class="notification-title">{{ title }}</p>
       <button class="close-button">
-        <img :src="closeIcon" alt="close" />
+        <img :src="CloseIcon" alt="close" />
       </button>
     </div>
     <div class="notification-content">
@@ -14,8 +14,30 @@
 </template>
 
 <script setup>
-import closeIcon from "@/assets/icons/Close.svg";
-import successIcon from "@/assets/icons/CheckCircle.svg";
+import { computed, defineProps } from "vue";
+import Info from "@/assets/icons/Info.svg";
+import Danger from "@/assets/icons/Danger.svg";
+import Warning from "@/assets/icons/Warning.svg";
+import CloseIcon from "@/assets/icons/Close.svg";
+import CheckCircle from "@/assets/icons/CheckCircle.svg";
+
+const NOTIFICATION_STATUS_CONFIG = {
+  success: { icon: CheckCircle, title: "Success" },
+  info: { icon: Info, title: "Info" },
+  warning: { icon: Warning, title: "Warning" },
+  error: { icon: Danger, title: "Error" },
+};
+
+const props = defineProps({
+  status: {
+    type: String,
+    required: true,
+  },
+});
+
+const config = computed(() => NOTIFICATION_STATUS_CONFIG[props.status]);
+const icon = computed(() => config.value.icon);
+const title = computed(() => config.value.title);
 </script>
 
 <style lang="scss" scoped>
@@ -24,38 +46,57 @@ import successIcon from "@/assets/icons/CheckCircle.svg";
   height: 160px;
   padding: 24px;
   border-radius: 5px;
-  background-color: #e4f7e8;
-  border: 2px solid #89b291;
+  border: 2px solid;
 
-  .notification-header {
-    display: flex;
-    align-items: center;
-    margin-bottom: 10px;
-
-    .notification-title {
-      font-weight: 500;
-      font-size: 20px;
-      flex: 1;
-    }
-
-    img {
-      margin-right: 16px;
-    }
-
-    .close-button {
-      cursor: pointer;
-      background: none;
-      border: none;
-      padding: 0;
-      width: 20px;
-      height: 20px;
-    }
+  &--success {
+    background-color: #e4f7e8;
+    border-color: #89b291;
   }
 
-  .notification-content {
-    font-size: 14px;
-    padding-left: 48px;
-    color: #757575;
+  &--error {
+    background-color: #f7e4e4;
+    border-color: #d29393;
   }
+
+  &--warning {
+    background-color: #f7f2e4;
+    border-color: #d4c296;
+  }
+
+  &--info {
+    background-color: #e4e9f7;
+    border-color: #96a8d8;
+  }
+}
+
+.notification-header {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+
+  .notification-title {
+    font-weight: 500;
+    font-size: 20px;
+    flex: 1;
+  }
+
+  img {
+    margin-right: 16px;
+  }
+
+  .close-button {
+    cursor: pointer;
+    background: none;
+    border: none;
+    padding: 0;
+    width: 20px;
+    height: 20px;
+  }
+}
+
+.notification-content {
+  font-size: 14px;
+  padding-left: 48px;
+  color: #757575;
 }
 </style>
