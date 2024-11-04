fc0dff336d4314565b0febef3420884d4ce248dd
Léa Cassegrain <lea.csgn@gmail.com>
2024-11-04 22:14:43 +0100

    feat: 1 - add BaseNotification component and related icons

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <BaseNotification>
+      <p>Operation completed successfully! 🌟</p>
+    </BaseNotification>
+  </main>
+</template>
+
+<script setup>
+import BaseNotification from "@/components/BaseNotification.vue";
+</script>
+
+<style scoped></style>
---
+++ src/components/BaseNotification.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="notification">
+    <div class="notification-header">
+      <img :src="successIcon" alt="icon" />
+      <p class="notification-title">Success</p>
+      <button class="close-button">
+        <img :src="closeIcon" alt="close" />
+      </button>
+    </div>
+    <div class="notification-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import closeIcon from "@/assets/icons/Close.svg";
+import successIcon from "@/assets/icons/CheckCircle.svg";
+</script>
+
+<style lang="scss" scoped>
+.notification {
+  width: 527px;
+  height: 160px;
+  padding: 24px;
+  border-radius: 5px;
+  background-color: #e4f7e8;
+  border: 2px solid #89b291;
+
+  .notification-header {
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+
+    .notification-title {
+      font-weight: 500;
+      font-size: 20px;
+      flex: 1;
+    }
+
+    img {
+      margin-right: 16px;
+    }
+
+    .close-button {
+      cursor: pointer;
+      background: none;
+      border: none;
+      padding: 0;
+      width: 20px;
+      height: 20px;
+    }
+  }
+
+  .notification-content {
+    font-size: 14px;
+    padding-left: 48px;
+    color: #757575;
+  }
+}
+</style>
